53378ec35adc0186ba38f3e481254c4234b27488
Marc Perez de Tudela <marcperezdetudelaaznar@gmail.com>
2024-05-29 21:40:19 +0200

    add author and books routes

--- lib/books_api_elixir_web/router.ex
+++ lib/books_api_elixir_web/router.ex
@@ -7,5 +7,8 @@ defmodule BooksApiElixirWeb.Router do
 
   scope "/api", BooksApiElixirWeb do
     pipe_through :api
+
+    resources "/authors", AuthorController, only: [:index, :create, :show, :update, :delete]
+    resources "/books", BookController, only: [:index, :create, :show, :update, :delete]
   end
 end
